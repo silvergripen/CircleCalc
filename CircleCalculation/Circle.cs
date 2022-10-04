c17f20834124fe4c943e41f682521b4be89bf168
silvergripen <Kenneth.hedberg.molen@gmail.com>
2022-10-04 17:00:54 +0200

    Lagt till constructor

--- CircleCalculation/Circle.cs
+++ CircleCalculation/Circle.cs
@@ -3,14 +3,17 @@
 public class Circle
 {
     
-        public float _Pi = 3.141f;
-    public int _Radius;
+    public float _Pi = 3.141f;
+    public int Radius;
 
+    public Circle(int Radius)
+    {
+       this.Radius = Radius;
+    }
     public float GetArea()
     {
-        float _Area = (_Radius * _Radius) * _Pi;
+        float _Area = (Radius * Radius) * _Pi;
         return _Area;
-
     }
 
 }
--- CircleCalculation/Program.cs
+++ CircleCalculation/Program.cs
@@ -7,13 +7,12 @@ namespace CircleCalculation
     {
         static void Main(string[] args)
         {
-            var instance = new Circle();
-            instance._Radius = 5;
-            Console.WriteLine("Arean för en cirkel med radien 5 är " + instance.GetArea() + " cm^2");
-            instance._Radius = 6;
-            Console.WriteLine("Arean för en cirkel med radien 6 är " + instance.GetArea() + " cm^2");
+            Circle c1 = new Circle(5);
+            Circle c2 = new Circle(6);
+            Console.WriteLine("Arean på cirkelnn när radien är {0} {1} cm^2", 5, c1.GetArea());
+            Console.WriteLine("Arean på cirkelnn när radien är {0} {1} cm^2", 6, c2.GetArea());
         }
+  
     }
-    //Console.WriteLine("Arean för en cirkel med radien 5 är " + instance.GetArea(5) + "cm^2");
-    //Console.WriteLine("Arean för en cirkel med radien 6 är " + instance.GetArea(6) + "cm^2"); 
+   
 }
