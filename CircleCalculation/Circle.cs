9edc1fcfe511712a198b172a1c320733a70d2e6a
Kenne <Kenne@DESKTOP-GD035FL>
2022-10-04 14:59:02 +0200

    Add project files.

---
+++ CircleCalculation/Circle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Circle
+{
+    
+        public float _Pi = 3.141f;
+    public int _Radius;
+
+    public float GetArea()
+    {
+        float _Area = (_Radius * _Radius) * _Pi;
+        return _Area;
+
+    }
+
+}
+    
+ 
+
+
+
---
+++ CircleCalculation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CircleCalculation
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var instance = new Circle();
+            instance._Radius = 5;
+            Console.WriteLine("Arean för en cirkel med radien 5 är " + instance.GetArea() + " cm^2");
+            instance._Radius = 6;
+            Console.WriteLine("Arean för en cirkel med radien 6 är " + instance.GetArea() + " cm^2");
+        }
+    }
+    //Console.WriteLine("Arean för en cirkel med radien 5 är " + instance.GetArea(5) + "cm^2");
+    //Console.WriteLine("Arean för en cirkel med radien 6 är " + instance.GetArea(6) + "cm^2"); 
+}
